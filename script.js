21287cb901c936cb81e242bb10b9b3c372a359f4
johanyim <johanjyyim@gmail.com>
2023-08-08 15:52:50 +0100

    doesn't exceed 8 digits anymore

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 const digits = document.getElementsByClassName("digit");
 const screen = document.getElementById("screentext")
 const ZERO = "&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp0"
-
+const MIN = -99999999
+const MAX =  99999999
 
 let workingNumber = 0;
 let currentNumber = 0;
+let decimalMode = false;
 let displayText = "&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp0"
 
 
@@ -23,33 +25,38 @@ function display(){
 
     //TODO: maximum number of digits on screen
     if(displayText.length > 8) {
-        
+        return
     }
 
-
-
-    for(let d = displayText.length; d <= 8; d++) {
+    for(let d = displayText.replace('.', '').length; d <= 8; d++) {
         displayText = "&nbsp" + displayText
     }
 
     screentext.innerHTML = displayText;
-
-
-
 }
 
-
+//gets the number of digits that would be displayed 
+function dCount(number) {
+    number.toString().replace('.', '').length
+}
 
 
 console.log(Array(digits))
 
-
-
+function enterDigit(d_str) {
+    d =  parseInt(d_str)
+    if(decimalMode){
+        currentNumber = 10*currentNumber + d;
+    }else{
+        currentNumber = 10*currentNumber + d;
+    }
+    
+}
 
 
 Array.from(digits).forEach(digit => {
     digit.addEventListener('click', () =>{
-        currentNumber = 10*currentNumber + parseInt(digit.id);
+        enterDigit(digit.id)
         // alert(digit.id)
         display();
     })
@@ -63,6 +70,17 @@ display()
 document.getElementById("ac").addEventListener('click', ()=> {    
     workingNumber = 0;
     currentNumber = 0;
+    decimalMode = false;
     displayText = ZERO;
     display();
 })
+
+
+// decimal point
+document.getElementById("point").addEventListener('click', ()=> {    
+    decimalMode = true; 
+})
+
+
+
+display()
